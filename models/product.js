d8d8d4797d744f6a0279de245a7d6394e1641447
Pranjali Rathi <pranjalirathi05@gmail.com>
2024-06-15 15:23:18 +0530

    database

--- models/product.js
+++ models/product.js
@@ -82,26 +82,27 @@
 
 
 // <----------------------------------------------------------------------------------------------------\
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
 const Cart = require('./cart');
+const db = require('../utils/database');
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
+// const p = path.join(
+//   path.dirname(process.mainModule.filename),
+//   'data',
+//   'products.json'
+// );
 
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
+// const getProductsFromFile = cb => {
+//   fs.readFile(p, (err, fileContent) => {
+//     if (err) {
+//       cb([]);
+//     } else {
+//       cb(JSON.parse(fileContent));
+//     }
+//   });
+// };
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
@@ -113,47 +114,58 @@ module.exports = class Product {
   }
 
   save() {
-    getProductsFromFile(products => {
-      if (this.id) {
-        const existingProductIndex = products.findIndex(
-          prod => prod.id === this.id
-        );
-        const updatedProducts = [...products];
-        updatedProducts[existingProductIndex] = this;
-        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-          console.log(err);
-        });
-      } else {
-        this.id = Math.random().toString();
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err => {
-          console.log(err);
-        });
-      }
-    });
+    // getProductsFromFile(products => {
+    //   if (this.id) {
+    //     const existingProductIndex = products.findIndex(
+    //       prod => prod.id === this.id
+    //     );
+    //     const updatedProducts = [...products];
+    //     updatedProducts[existingProductIndex] = this;
+    //     fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+    //       console.log(err);
+    //     });
+    //   } else {
+    //     this.id = Math.random().toString();
+    //     products.push(this);
+    //     fs.writeFile(p, JSON.stringify(products), err => {
+    //       console.log(err);
+    //     });
+    //   }
+    // });
+
+    //first question marks are added and then that
+    //it is beacuse mysql will then safely escape our input values to basically parse it for a hidden SQL command and remove them and hence there will be a extra security layer
+    return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES ( ?, ?, ?, ?)' ,
+      [this.title, this.price, this.imageUrl, this.description]  
+    );
   }
 
   static deleteById(id) {
-    getProductsFromFile(products => {
-      const product = products.find(prod => prod.id === id);
-      const updatedProducts = products.filter(prod => prod.id !== id);
-      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-        if (!err) {
-          Cart.deleteProduct(id, product.price);
-        }
-      });
-    });
+    // getProductsFromFile(products => {
+    //   const product = products.find(prod => prod.id === id);
+    //   const updatedProducts = products.filter(prod => prod.id !== id);
+    //   fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+    //     if (!err) {
+    //       Cart.deleteProduct(id, product.price);
+    //     }
+    //   });
+    // });
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
+  static fetchAll() {
+    // getProductsFromFile(cb);
+    return db.execute('SELECT * FROM products');
   }
 
-  static findById(id, cb) {
-    getProductsFromFile(products => {
-      const product = products.find(p => p.id === id);
-      cb(product);
-    });
-  }
-};
+  static findById(id) {
+  //   getProductsFromFile(products => {
+  //     const product = products.find(p => p.id === id);
+  //     cb(product);
+  //   });
+  // }
 
+  return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
+
+  };
+
+}
--- utils/database.js
+++ utils/database.js
@@ -4,8 +4,14 @@ const mysql = require('mysql2');
 //craeting a pool of connections which will work whenevr we have a query to run
 //each quuery needs its conetcion
 //once a query is done teh connection will be handed back into the pool and it is available to a new query
-
+//passing some information about the databse object
 const pool = mysql.createPool({
     host : 'localhost',
-    user: 
+    user: 'root',
+    database: 'node-first-app',
+    password: 'root'
 })
+
+///exporting in the form of promises to handle asyncrnous code instaed of callbacks
+
+module.exports = pool.promise();
